365256d7097778d2a8b5fb135ef67d930ab153ac
AbhishekJha-45 <krjha36555@gmail.com>
2023-07-26 12:43:32 +0530

    ecosystem file added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       "post-deploy":
         "source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
-      ssh_options: "forwardAgent=yes",
+      ssh_options: "ForwardAgent=yes",
     },
   },
 };
